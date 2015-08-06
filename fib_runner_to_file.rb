a78252b43418927d39e412cd1ba8758f808a67c9
Jacob Oakes <jacoboakes92@gmail.com>
2015-08-06 18:27:44 -0400

    added making graph to compare fibonacci numbers

--- fib_runner_to_file.rb
+++ fib_runner_to_file.rb
@@ -1,21 +1,44 @@
 require 'benchmark'
+require 'gruff'
 
-def run_cmd(cmd)
-	`#{cmd}` or raise "The following command failed to run #{cmd}"
-end	
+class FibRunner
+	def run_cmd(cmd)
+		Kernel.system(cmd) or raise "The following command failed to run #{cmd}"
+	end	
 
-file = File.open('crystal-vs-ruby-fib.csv', 'w')
-file.puts "Fib, Crystal Time, Ruby Time\n"
-run_cmd('crystal build fib.cr --release')
+	def run(n)
+		run_cmd('crystal build fib.cr --release')
+		crystal_times = []
+		ruby_times = []
 
-(0..40).each do |n|
-	crystal_time = Benchmark.realtime do 
-	  run_cmd("./fib #{n}")
+		(0..n).each do |n|
+			crystal_time = Benchmark.realtime do 
+			  run_cmd("./fib #{n}")
+			end
+
+			ruby_time =  Benchmark.realtime do 
+			  run_cmd("ruby fib.rb #{n}")
+			end
+			crystal_times.push(crystal_time)
+			ruby_times.push(ruby_time)
+		end	
+
+		g = Gruff::Line.new(2000)
+		g.title = "Fibonacci 0 - #{n}"
+		g.labels = labels(0, n)
+		g.data(:ruby, ruby_times)
+		g.data(:crystal, crystal_times)
+		g.write("fibonacci-#{n}.png")
 	end
 
-	ruby_time =  Benchmark.realtime do 
-	  run_cmd("ruby fib.rb #{n}")
-	end
-	file.puts "#{n},#{crystal_time},#{ruby_time}\n"
-end	
-file.close()
+	def labels(min_x, max_x)
+		delta = (max_x - min_x) / 3
+		labels = {}
+		(min_x..max_x).step(delta) do |n|
+		    labels[n] = n.to_s
+		end
+		labels
+	end	
+end
+
+FibRunner.new.run(30)
