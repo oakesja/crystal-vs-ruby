cf6eb2647661dfac12da8413d5b2656ab7d519a4
Jacob Oakes <jacoboakes92@gmail.com>
2015-09-14 22:48:07 -0400

    added valid images

--- fib/fib_runner_to_file.rb
+++ fib/fib_runner_to_file.rb
@@ -10,23 +10,22 @@ class FibRunner
 		run_cmd('crystal build fib.cr --release')
 		crystal_times = benchmark_up_to(n) { |i| run_cmd("./fib #{i}") }
 		cleanup
-		# ruby_times = benchmark_up_to(n) { |i| run_cmd("ruby fib.rb #{i}") }
+		ruby_times = benchmark_up_to(n) { |i| run_cmd("ruby fib.rb #{i}") }
 		# jruby_times = benchmark_up_to(n) { |i| run_cmd("jruby fib.rb #{i}") }
 		# rubinius_times = benchmark_up_to(n) { |i| run_cmd("~/rubinius/bin/rbx fib.rb #{i}") }
-		run_cmd('gcc fib.c -o fib')
-		c_times = benchmark_up_to(n) { |i| run_cmd("./fib #{i}") }
-		cleanup
+		# run_cmd('gcc fib.c -o fib')
+		# c_times = benchmark_up_to(n) { |i| run_cmd("./fib #{i}") }
+		# cleanup
 
 		g = Gruff::Line.new(800)
 		g.title = "Fibonacci 0 - #{n}"
 		g.labels = labels(0, n)
-		# g.data(:ruby , ruby_times)
+		g.data(:ruby , ruby_times)
 		g.data(:crystal, crystal_times)
 		# g.data(:jruby, jruby_times)
 		# g.data(:rubinius, rubinius_times)
-		g.data(:c, c_times)
-		g.write("fibonacci-#{n}-c-crystal
-.png")
+		# g.data(:c, c_times)
+		g.write("fibonacci-#{n}-ruby-crystal.png")
 	end
 
 	def benchmark_up_to(n)
